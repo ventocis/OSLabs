1970aa52c19594892095c192f7b9033e3d4434d3
Mazen Ashgar <Mazen.ashgar@gmail.com>
2020-01-20 17:45:32 -0500

    Added the resource calculation part by the parent TODO: calling a program with arguments doesn't always work

--- labProgram.c
+++ labProgram.c
@@ -4,63 +4,72 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-
+#include <sys/time.h>
+#include <sys/resource.h>
+/**********************************************
+*This program is a simple shell that takes the
+*user input and execute it in a child process.
+*
+*@Author: Samuel Ventocilla & Mazen Ashgar
+**********************************************/
 int main(int argc, char** argv){
-	char command[100];
-	char** commandList = NULL;
-	char* quit = "quit\n";
-	int pid = -2;
-	char** pCommandList;
 
-	while (1) {
-		printf("\nEnter your command or type quit to exit\n");
-		fgets(command, 100, stdin);
+  //declare and initialize variables
+  char command[100];
+  char** commandList = NULL;
+  char* quit = "quit\n";
+  int pid = -2;
+  int status;
+  struct rusage resources;
 
-		//exit if user types quit
-		if (strcmp(command, quit) == 0)
-			exit(0);
+  //keep getting user input until "quit"
+  while (1) {
 
-		//remove newline from string
-		command[strlen(command) - 1] = 0;
+    //prompt user for input and store input
+    printf("\nEnter your command or type quit to exit\n");
+    fgets(command, 100, stdin);
 
-		int cout = 0
-		//Tokenize string & store into dynamically allocated string
-		for (char * p = strtok(command, " "); p != NULL; p = strtok(NULL, " ")){
-				count++;
-				commandList = (char**)realloc(commandList, (count + 1)*sizeof(*commandList));
-				commandList[count-1] = (char*)malloc(sizeof(p));
-				strcpy(commandList[count-1], p);
-		}
+    //exit if user types quit
+    if (strcmp(command, quit) == 0){
+      free(commandList);
+      exit(0);
+    }
 
-		//saved the PID to a variable so that we can get info using it in commands later
-		pid = fork();
+    //remove newline from string
+    command[strlen(command) - 1] = 0;
 
-		//child runs this
-		if(pid == 0){
-				execvp(commandList[0], commandList);
-				exit(0);
-		}
-		//parent runs this
-		else{
-		//Tried to make this work, but wasn't successful. Not sure if it's the right track or not
-		/*
-			char pCommand[100] = "grep ctxt /proc/";
-			char strPid[50];
-			sprintf(strPid, "%d", pid);
-			strcat(pCommand, strPid);
-			strcat(pCommand, "/status");
+    int count = 0;
 
-			wait(NULL);
-			int check = 0;
-			for (char * pP = strtok(pCommand, " "); pP != NULL; pP = strtok(NULL, " ")){
-				check++;
-				pCommandList = (char**)realloc(pCommandList, (check+1)*sizeof(*pCommandList));
-				pCommandList[check-1] = (char*)malloc(sizeof(pP));
-				strcpy(pCommandList[check-1], pP);
-			}
-			execvp(pCommandList[0], pCommandList);
-			*/
-			wait(NULL);
-		}
-	}
+    //Tokenize string & store into dynamically allocated string
+    for (char * p = strtok(command, " "); p != NULL; p = strtok(NULL, " ")){
+      count++;
+      commandList = (char**)realloc(commandList, (count + 1)*sizeof(*commandList));
+      commandList[count-1] = (char*)malloc(sizeof(p));
+      strcpy(commandList[count-1], p);
+    }
+
+    //saved the PID to a variable so that we can get info using it in commands later
+    pid = fork();
+
+    //child runs this
+    if(pid == 0){
+      if(execvp(commandList[0], commandList) == -1){
+        printf("Incorrect command\n");
+      }
+      exit(0);
+    }
+
+    //parent runs this
+    else{
+
+      //wait for child process to end
+      waitpid(-1 ,&status, 0);
+
+      //get rusage from child process
+      getrusage(RUSAGE_CHILDREN, &resources);
+
+      //print the usage time to the user
+      printf("CPU usage: %d ms", resources.ru_utime.tv_usec);
+      }
+    }
 }
